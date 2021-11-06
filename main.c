38b4e705d1214591810c22b928f2c604a1345c71
Joao Pedro <joaopedrorib@hotmail.com>
2021-11-05 21:15:22 -0300

    resolve conflicts

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ int main()
 {
     LISTA *catalogo;
     catalogo = lerJogosDoCSV();
-<<<<<<< Updated upstream
 
     char input[2];
     scanf("%s",input);
@@ -57,14 +56,6 @@ int main()
     }
     return 0;
 
-=======
-    lista_imprimir_todos_jogos(catalogo);
-    //printf("\n\n");
-    //lista_imprimir_jogo_from_index(catalogo, 5);
-    lista_mover_esquerda(catalogo, 0, 6);
-    printf("\n----------\n");
-    lista_imprimir_todos_jogos(catalogo);
->>>>>>> Stashed changes
     //testeEmpilhaStdin();
     //testeInserirRemover();
     //testePesquisas();
